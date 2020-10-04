b7c179ddc74179b070c9f99ebbe0cc18a94883a1
eguoli <guoli@bk.ru>
2020-10-04 14:12:44 +0000

    Update with some minor changes

--- README.md
+++ README.md
@@ -8,19 +8,20 @@ apt-get install curl jq # Debian/Ubuntu
 yum install curl jq # CentOS
 ```
 
-2. Set the variables in mysent.passwd:
+2. Set the variables in mysent.conf:
 
 ```bash
-GOBIN="" # Sentinel cli folder with full path, ie /root/go/bin
+GOBIN="" # Sentinel cli folder with full path, e.g. /root/go/bin
 ADDR="" # Sentinel wallet address
 OPER="" # Sentinel validator address
 NODE="" # Sentinel node name
 PASS="" # Sentinel wallet password
 CLAIM=100 # Minimum rewards amount to withdraw in TSENT
 DELEGATE=1000 # Minimum amount to delegate in TSENT
+SLEEP=10 # Wait for N seconds for the transaction to be confirmed so we get an updated balance
 
 BOT="" # Telegram bot api from @BotFather
-TGID="" # Telegram Chat ID
+TGID="" # Your Telegram ID
 ```
 - To get the Telegram bot API key just talk to @BotFather and set up your own bot.
 - To define your Telegram ID send a message to your newly created bot and open in browser https://api.telegram.org/botINSERTAPIKEY/getUpdates - in the json string returned you will find your "id".
@@ -28,7 +29,7 @@ TGID="" # Telegram Chat ID
 3. Make the file with your variables not readable for anyone but owner
 
 ```bash
-chmod 400 mysent.passwd
+chmod 400 mysent.conf
 ```
 
 4. Make the main script executable
@@ -51,3 +52,6 @@ crontab -u <user> -e
 ```
 This setting will run a script every hour at :00 minutes. Set as */10 instead to run this job every 10 minutes.
 
+---
+
+Please feel free to contact me @eguoli on Telegram ;)
--- mysent.sh
+++ mysent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Import the variables from mysent.passwd
-. mysent.passwd
+# Import the variables from mysent.conf
+. mysent.conf
 
 # Get validator voting power info
 power=$($GOBIN/sentinel-hub-cli status --chain-id sentinel-turing-3a --output json | jq -r '.validator_info .voting_power')
@@ -10,38 +10,39 @@ rewards=$($GOBIN/sentinel-hub-cli query distribution rewards $ADDR $OPER --chain
 amount=${rewards%.*}
 
 # Compose message text with emojis in Unicode https://apps.timwhitlock.info/emoji/tables/unicode
-TEXT=""$'\U0001F4AB'"%20Rewards:%20"
+TEXT=""$'\U0001F50B'"%20Voting%20Power:%20$power%0A"
+TEXT+=""$'\U0001F4AB'"%20Rewards:%20"
 TEXT+=$(printf %.4f $(echo "$amount/1000000" | bc -l))
 TEXT+=" TSENT"
 
 # Withdraw rewards
 if (($amount > $CLAIM*1000000)); then
 	echo $PASS | $GOBIN/sentinel-hub-cli tx distribution withdraw-rewards $OPER --commission --from $NODE --chain-id sentinel-turing-3a -y
-	TEXT+="%0AWithdraw%20rewards%20%26%20commission"
+	TEXT+="%20"$'\U000027A1'"%20withdraw"
 fi
 
-# Wait for tx to be confirmed so we get the updated balance
-sleep 8
+# Wait for tx to be confirmed so we get an updated balance
+sleep $SLEEP
 
 # Get available balance
 checkbal=$($GOBIN/sentinel-hub-cli query account $ADDR --chain-id sentinel-turing-3a --output json | jq -r '.value .coins[0] .amount')
 balance=${checkbal%.*}
 
-MSG=""$'\U0001F50B'"%20Voting%20Power:%20$power%0A"$'\U0001F4B0'"%20Available:%20"
-MSG+=$(printf %.4f $(echo "$balance/1000000" | bc -l))
-MSG+=" TSENT%0A"
+TEXT+="%0A"$'\U0001F4B0'"%20Available:%20"
+TEXT+=$(printf %.4f $(echo "$balance/1000000" | bc -l))
+TEXT+=" TSENT"
 
 # Delegate available balance to validator
 if (($balance > $DELEGATE*1000000)); then
-	# Leave 1 TSENT and delegate the rest 
+	# Leave 1 TSENT and delegate the rest
 	SUM=$((balance-1000000))
 	echo $PASS | $GOBIN/sentinel-hub-cli tx staking delegate $OPER ${SUM}tsent --from $NODE --chain-id sentinel-turing-3a -y
-	TEXT+="%0ADelegate%20"
+	TEXT+="%20"$'\U0001F501'"%20delegate%20"
 	TEXT+=$(printf %.4f $(echo "$SUM/1000000" | bc -l))
 	TEXT+=" TSENT"
 fi
 
 # Send message via Telegram
 send=$(curl -s -X POST -H "Content-Type:multipart/form-data" \
-    "https://api.telegram.org/bot$BOT/sendMessage?chat_id=$TGID&text=$MSG$TEXT")
+    "https://api.telegram.org/bot$BOT/sendMessage?chat_id=$TGID&text=$TEXT")
 
